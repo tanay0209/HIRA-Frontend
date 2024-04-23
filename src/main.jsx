7e98c40d5ffc5d45613a4ce5cc545fa6f8b37b6e
tanay0209 <tanayjagnani@gmail.com>
2024-04-23 21:58:52 +0530

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from "@material-tailwind/react";
 import React from 'react';
-import { Provider } from 'react-redux'
-import store from './store/store.js'
+// import { Provider } from 'react-redux'
+// import store from './store/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </Provider>
+    {/* <Provider store={store}> */}
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+    {/* </Provider> */}
   </React.StrictMode>,
 )
